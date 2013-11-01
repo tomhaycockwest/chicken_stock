3a3e21678d1b1bc84d358d4c62419d788068d602
Tom Haycock-West <tomhaycockwest@gmail.com>
2013-11-01 21:48:11 +0000

    create meat titles

--- js/card.js
+++ js/card.js
@@ -4,13 +4,3 @@ $('#container').isotope({
     rowHeight: 248
   }
 });
-
-// $('#container').isotope({ layoutMode : 'fitRows', columnWidth: 120});
-
-// $('#container').isotope({
-//   layoutMode: 'cellsByRow',
-//   cellsByColumn: {
-//     columnWidth: 440,
-//     rowHeight: 360
-//   }
-// });
